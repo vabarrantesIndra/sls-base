2464bf31bf48cdc9ef481ba068a60f72f228a76a
vabarrantesIndra <vabarrantes@indracompany.com>
2024-11-26 09:55:19 -0500

    add middy

--- src/handlers/createAuction.mjs
+++ src/handlers/createAuction.mjs
@@ -1,11 +1,15 @@
 import {v4 as uuidv4} from 'uuid';
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
-
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpErrorHandler from '@middy/http-error-handler';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import createError from 'http-errors';
 
 const client = new DynamoDBClient({region: 'us-east-1'});
 async function createAuction(event, context) {
 
-  const {title} = JSON.parse(event.body);
+  const {title} = event.body
 
   const auction = {
     id: uuidv4(),
@@ -14,18 +18,23 @@ async function createAuction(event, context) {
     createdAt: new Date().toISOString(),
   }
 
-  const dynamoResponse = await client.send(new PutItemCommand({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: marshall(auction),
-  }));
+  try {
+    await client.send(new PutItemCommand({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: marshall(auction),
+    }));
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
-console.log('dynamoResponse:', dynamoResponse);
+
   return {
     statusCode: 200,
     body: JSON.stringify(auction),
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction).use(httpJsonBodyParser()).use(httpEventNormalizer()).use(httpErrorHandler());
 
 

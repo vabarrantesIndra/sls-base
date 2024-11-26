76c26ccac4517e18fdc47926de1d8160d456d660
vabarrantesIndra <vabarrantes@indracompany.com>
2024-11-26 08:18:21 -0500

    refactor of IAMrole and table resources to separate folders

---
+++ src/handlers/createAuction.mjs
@@ -0,0 +1,31 @@
+import {v4 as uuidv4} from 'uuid';
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+
+
+const client = new DynamoDBClient({region: 'us-east-1'});
+async function createAuction(event, context) {
+
+  const {title} = JSON.parse(event.body);
+
+  const auction = {
+    id: uuidv4(),
+    title,
+    status: 'OPEN',
+    createdAt: new Date().toISOString(),
+  }
+
+  const dynamoResponse = await client.send(new PutItemCommand({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: marshall(auction),
+  }));
+
+console.log('dynamoResponse', dynamoResponse);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
+
+

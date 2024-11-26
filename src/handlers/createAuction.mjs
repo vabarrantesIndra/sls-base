ee3af86bb7a19484a3dab2e38a9eae4abea4aca2
vabarrantesIndra <vabarrantes@indracompany.com>
2024-11-26 08:20:23 -0500

    test

--- src/handlers/createAuction.mjs
+++ src/handlers/createAuction.mjs
@@ -19,7 +19,7 @@ async function createAuction(event, context) {
     Item: marshall(auction),
   }));
 
-console.log('dynamoResponse', dynamoResponse);
+console.log('dynamoResponse:', dynamoResponse);
   return {
     statusCode: 200,
     body: JSON.stringify(auction),
